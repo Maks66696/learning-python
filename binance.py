bdf2e7b3ad4514be102319ea6d409b01eacf42f4
Maks66696 <koryakin_10@internet.ru>
2026-07-30 12:22:49 +0500

    feat: добавлена возможность сохранения найденных монет в JSON-файл

--- binance.py
+++ binance.py
@@ -1,10 +1,11 @@
-
+import json
 import requests
-
+coins_keys={}
 
 while True:
     print("[1]-Узнать курс монеты")
-    print("[2]-Выйти из программы")
+    print("[2]-Записать значения монет в json")
+    print("[3]-Выйти из программы")
     print("------------------------")
     choice = input("Введите номер: ")
     if choice == "1":
@@ -17,11 +18,22 @@ while True:
                 price=float(i['price'])
                 print(price)
                 found=True
+                coins_keys[coin] = price
                 break
         if not found:
             print("Ошибка: Монета не найдена. Проверьте правильность названия!")
+
+
     elif choice == "2":
+       if not coins_keys:
+           print("Сначала найдите хотя бы одну монету через пункт [1]!")
+       else:
+            with open("coins_keys.json", "w") as file:
+                json.dump(coins_keys, file, indent=4)
+                print("Данные успешно сохранены в coins_keys.json!")
+    elif choice == "3":
         break
+
     else:
         print("Введите правильный номер!")
             
