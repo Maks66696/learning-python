e20da169e75f756fd182ad763d8a20608113a25d
Maks66696 <koryakin_10@internet.ru>
2026-08-09 15:29:01 +0500

    Добавлены советы для более жаркой погоды. А также добавлена функция реверс геокодирования

--- weather_bot.py
+++ weather_bot.py
@@ -1,16 +1,16 @@
 import requests
+import geopy
+from geopy.geocoders import Nominatim
 from telebot import TeleBot
 from telebot.types import KeyboardButton, ReplyKeyboardMarkup
 
-TOKEN = "..."
+TOKEN = "8640334347:AAFWE9Rp-hFBxonMYR9ZMV4VRJFCkAbNyVw"
 bot = TeleBot(TOKEN)
-
 
 CITIES = {
     "Москва": {"lat": 55.7558, "lon": 37.6173},
     "Санкт-Петербург": {"lat": 59.9343, "lon": 30.3351},
     "Екатеринбург": {"lat": 56.8389, "lon": 60.6057},
-    "Сосьва": {"lat": 59.174, "lon": 61.849},
     "Сочи": {"lat": 43.6028, "lon": 39.7342},
 }
 
@@ -42,8 +42,10 @@ def send_weather(message):
         advice = "Прохладно. Надевай куртку или тёплую худи."
     elif temp < 22:
         advice = "Приятная погода. Подойдёт кофта или ветровка."
-    else:
+    elif temp > 25:
         advice = "Жара! Надевай футболку и шорты."
+    elif temp > 35:
+        advice = "Экстремальная жара! Не выходи на солнце без кепки и пей больше воды!"
     text= f"Погода в **{city_name}**: {temp}°C\n\n{advice}"
     bot.send_message(message.chat.id, text, parse_mode="Markdown")
 
@@ -52,6 +54,10 @@ def send_weather(message):
 def handel_location(message):
     lat = message.location.latitude
     lon = message.location.longitude
+    geolocator = Nominatim(user_agent="my_weather_bot")
+    location = geolocator.reverse((lat, lon), language='ru')
+    address = location.raw.get('address', {})
+    city_name = address.get('city') or address.get('town') or address.get('village') or "вашей локации"
     url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
     response=requests.get(url)
     data=response.json()
@@ -62,9 +68,13 @@ def handel_location(message):
         advice = "Прохладно. Надевай куртку или тёплую худи."
     elif temp < 22:
         advice = "Приятная погода. Подойдёт кофта или ветровка."
-    else:
+    elif temp > 25:
         advice = "Жара! Надевай футболку и шорты."
-    text= f"Погода по вашей геолокации:{temp}°C\n\n{advice}"
+    elif temp > 35:
+        advice = "Экстремальная жара! Не выходи на солнце без кепки и пей больше воды!"
+    text = f"Погода по вашей геолокации:{temp}°C\n\n{advice}"
+    text = f"Погода в {city_name}: {temp}°C\n\n{advice}"
     bot.send_message(message.chat.id, text, parse_mode="Markdown")
     
 bot.infinity_polling()
+
