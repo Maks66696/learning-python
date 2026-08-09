08cf46ed3533735bbf782124dd2d11e04666f9e2
Maks66696 <koryakin_10@internet.ru>
2026-08-09 14:58:26 +0500

    Добавил функцию определения геолокации

--- weather_bot.py
+++ weather_bot.py
@@ -19,10 +19,13 @@ bot = TeleBot(TOKEN)
 @bot.message_handler(commands=["start", "help"])
 def send_welcome(message):
     markup = ReplyKeyboardMarkup(row_width=1)
+    btn_location = KeyboardButton("Отправить геолокацию", request_location=True)
+    markup.add(btn_location)
     for city in CITIES.keys():
         item_button = KeyboardButton(city)
         markup.add(item_button)
     bot.send_message(message.chat.id, "Выбери город", reply_markup=markup)
+
 
 @bot.message_handler(func=lambda message: message.text in CITIES.keys())
 def send_weather(message):
@@ -40,9 +43,28 @@ def send_weather(message):
     elif temp < 22:
         advice = "Приятная погода. Подойдёт кофта или ветровка."
     else:
-        advice = " Жара! Надевай футболку и шорты."
+        advice = "Жара! Надевай футболку и шорты."
     text= f"Погода в **{city_name}**: {temp}°C\n\n{advice}"
-
     bot.send_message(message.chat.id, text, parse_mode="Markdown")
 
+
+@bot.message_handler(content_types=["location"])
+def handel_location(message):
+    lat = message.location.latitude
+    lon = message.location.longitude
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
+    response=requests.get(url)
+    data=response.json()
+    temp=data["current_weather"]["temperature"]
+    if temp < 0:
+        advice = "На улице мороз! Надевай пуховик, шапку и шарф."
+    elif temp < 15:
+        advice = "Прохладно. Надевай куртку или тёплую худи."
+    elif temp < 22:
+        advice = "Приятная погода. Подойдёт кофта или ветровка."
+    else:
+        advice = "Жара! Надевай футболку и шорты."
+    text= f"Погода по вашей геолокации:{temp}°C\n\n{advice}"
+    bot.send_message(message.chat.id, text, parse_mode="Markdown")
+    
 bot.infinity_polling()
